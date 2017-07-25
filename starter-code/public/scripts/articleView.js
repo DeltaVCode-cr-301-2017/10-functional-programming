9273601c2eda515eddb2acc56803cb8b85792122
ByteMiBro <bytemibro@gmail.com>
2017-07-25 17:50:05 -0500

    turned html template into list

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -138,7 +138,7 @@ var app = app || {};
     // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
     // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
     // we are then calling "template" on line 117.
-    var source   = $('#author-template').html();
+    var source   = $('#author-stats').html();
     var template = Handlebars.compile(source);
     // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
     // appending to the DOM.
